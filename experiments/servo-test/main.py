87dbb9ac76ca7dfed97116b62393fa235610870f
Billy Au <billy@billyau.net>
2026-04-01 16:35:01 +0800

    servo-test: home=500us, 5 steps to 2000us, 300us per step

--- experiments/servo-test/main.py
+++ experiments/servo-test/main.py
@@ -1,29 +1,36 @@
 from microbit import pin0, button_a, button_b, display, sleep
 
-# Find min and max pulse width for your servo.
-# A = increase pulse (move one direction)
-# B = decrease pulse (move other direction)
-# Scroll shows current pulse width after each press.
-
+HOME_US   = 500
+MAX_US    = 2000
+STEPS     = 5
+STEP_US   = (MAX_US - HOME_US) // STEPS   # 300us per step
 PERIOD_US = 20000
-pos = 1500
+SETTLE_MS = 500
 
+# Slot 0: 500us  Slot 1: 800us  Slot 2: 1100us
+# Slot 3: 1400us  Slot 4: 1700us  Slot 5: 2000us
 
 def set_servo(us):
     pin0.set_analog_period_microseconds(PERIOD_US)
     pin0.write_analog(int(us / PERIOD_US * 1023))
 
 
-set_servo(pos)
-display.scroll(str(pos), delay=60)
+slot = 0
+set_servo(HOME_US)
+sleep(SETTLE_MS)
+display.show("0")
 
 while True:
-    if button_a.was_pressed():
-        pos += 50
-        set_servo(pos)
-        display.scroll(str(pos), delay=60)
-    elif button_b.was_pressed():
-        pos -= 50
-        set_servo(pos)
-        display.scroll(str(pos), delay=60)
+    if button_a.is_pressed() and button_b.is_pressed():
+        slot = 0
+        set_servo(HOME_US)
+        sleep(SETTLE_MS)
+        display.show("0")
+        sleep(800)
+    elif button_a.was_pressed():
+        if slot < STEPS:
+            slot += 1
+            set_servo(HOME_US + slot * STEP_US)
+            sleep(SETTLE_MS)
+            display.show(str(slot))
     sleep(50)
